6fdfefecd9fdb225ab3670b40d8643bf44dbc565
Elian R Pinheiro <79043676+ElianPinheiro12@users.noreply.github.com>
2025-06-25 16:23:49 -0300

    Update and rename Exten.v to extensor_sinal.v

--- Verilog/Exten.v
+++
@@ -1,9 +0,0 @@
-module sign_extender (
-    input wire [15:0] in,           // Imediato de 16 bits (entrada)
-    output wire [31:0] out          // Saída estendida para 32 bits
-);
-
-    // Estende o sinal: se o bit 15 for 1, completa com 1s; senão, com 0s
-    assign out = {{16{in[15]}}, in};
-
-endmodule
